f8093f5cb76733cf4f3acdf64b34783acb01065e
Andrus Rinde <andrusrinde@users.noreply.github.com>
2022-09-17 09:21:16 +0300

    Teine kodune ülesanne

---
+++ page.php
@@ -0,0 +1,52 @@
+<?php
+	$author_name = "Andrus Rinde";
+	$full_time_now = date("d.m.Y H:i:s");
+	$weekday_now = date("N");
+	//echo $weekday_now;
+	$weekdaynames_et = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
+	//echo $weekdaynames_et[$weekday_now - 1];
+	$hours_now = date("H");
+	//echo $hours_now;
+	$part_of_day = "suvaline päeva osa";
+	//  <   > >=  <=   ==  !=
+	if($hours_now < 7){
+		$part_of_day = "uneaeg";
+	}
+	//   and   or
+	if($hours_now >= 8 and $hours_now < 18){
+		$part_of_day = "koolipäev";
+	}
+	
+	//uurime semestri kestmist
+	$semester_begin = new DateTime("2022-9-5");
+	$semester_end = new DateTime("2022-12-18");
+	$semester_duration = $semester_begin->diff($semester_end);
+	//echo $semester_duration;
+	$semester_duration_days = $semester_duration->format("%r%a");
+	$from_semester_begin = $semester_begin->diff(new DateTime("now"));
+	$from_semester_begin_days = $from_semester_begin->format("%r%a");
+	
+	//juhuslik arv
+	//küsin massiivi pikkust
+	//echo count($weekdaynames_et);
+	//echo mt_rand(0, count($weekdaynames_et) -1);
+	//echo $weekdaynames_et[mt_rand(0, count($weekdaynames_et) -1)];
+	
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $author_name;?> programmeerib veebi</title>
+</head>
+<body>
+<img src="pics/vp_banner_gs.png" alt="bänner">
+<h1><?php echo $author_name;?> programmeerib veebi</h1>
+<p>See leht on loodud õppetöö raames ja ei sisalda tõsiseltvõetavat sisu!</p>
+<p>Õppetöö toimus <a href="https://www.tlu.ee" target="_blank">Tallinna Ülikoolis</a> Digitehnoloogiate instituudis.</p>
+<p>Lehe avamise hetk: <?php echo $weekdaynames_et[$weekday_now - 1] .", " .$full_time_now;?></p>
+<p>Praegu on <?php echo $part_of_day;?>.</p>
+<p>Semestri pikkus on <?php echo $semester_duration_days;?> päeva. See on kestnud juba <?php echo $from_semester_begin_days; ?> päeva.</p>
+<img src="pics/tlu_39.jpg" alt="Tallinna Ülikooli ajalooline Terra õppehoone">
+</body>
+</html>
